477640931ae18f2e9f5ec64162a0fcfa44df3d31
Harsh Pratap Singh <harsh.pratap@oges.co>
2023-12-28 18:44:08 +0530

    form array binding

--- src/app/pages/form-array/form-array.component.ts
+++ src/app/pages/form-array/form-array.component.ts
@@ -1,32 +1,41 @@
 import { Component } from '@angular/core';
 import { UntypedFormBuilder, Validators, UntypedFormGroup, FormArray, AbstractControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import monthsArr from "../../../assets/json/months.json";
+import monthsJson from "../../../assets/json/months.json";
 
 @Component({
   selector: 'app-form-array',
   templateUrl: './form-array.component.html',
   styleUrls: ['./form-array.component.scss']
 })
+
+
 export class FormArrayComponent {
   public addUserForm!: UntypedFormGroup;
   public monthsArr: any;
+  public selectedYear: any;
 
   constructor(public formBuilder: UntypedFormBuilder, public route: ActivatedRoute) {
-    this.monthsArr = monthsArr;
+    this.monthsArr = monthsJson.months;
   }
 
 
   ngOnInit() {
     this.addUserForm = this.formBuilder.group({
-      name: ['', []],
-      email: ['', []],
-      monthlyData: this.formBuilder.array([])
-    })
-
-    this.monthsArr.months.forEach((month: any) => {
-      this.monthlyDataArray.push(this.formBuilder.control(''));
+      year: ['', []],
+      monthlyData: this.formBuilder.array(
+        this.monthsArr.map((month: any) => this.createCapValue(month))
+      )
     });
+  }
+
+  createCapValue(month: any) {
+    const monthGroup = this.formBuilder.group({
+      month: [month.value, []], // You can remove this line if not needed
+      value: ['', []],
+    });
+
+    return monthGroup;
   }
 
 
@@ -34,12 +43,12 @@ export class FormArrayComponent {
     return this.addUserForm.controls;
   }
 
-  get monthlyDataArray(): any {
-    return this.addUserForm.get('monthlyData') as FormArray;
+  get monthData(): FormArray {
+    return <FormArray>this.addUserForm.get('monthlyData');
   }
 
 
-  createUser() {
+  printData(index: any) {
     console.log("add user form =>", this.addUserForm);
 
     this.addUserForm.value['']
